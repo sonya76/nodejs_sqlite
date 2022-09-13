67c370b969544bbbaeda221ab5ab3a91e02b6c13
Sonia Zaïna CHALAL <soniachalalceppic@gmail.com>
2022-09-13 16:39:47 +0200

    create table + insert + delete + ajout champ date

---
+++ index.js
@@ -0,0 +1,22 @@
+const sqlite3 = require("sqlite3").verbose();
+const dbname='univers.db';
+let db = new sqlite3.Database(dbname, err => {
+    if(err)
+    throw err
+    console.log("Base de donnée initialisé dans le fichier : " + dbname);
+});
+
+//db.run('CREATE TABLE IF NOT EXISTS planetes(id INTEGER PRIMARY KEY, name,size VARCHER(25))');
+//db.run('CREATE TABLE IF NOT EXISTS planetes(id INTEGER PRIMARY KEY, name VARCHAR(25), size VARCHAR(255), created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL)');
+
+db.run('INSERT INTO planetes(name,size) values (?,?)', ["Vénus", "352158"]);
+db.run('INSERT INTO planetes(name,size) values (?,?)', ["Terre", "8516975125"]);
+db.run('INSERT INTO planetes(name,size) values (?,?)', ["Pluton","65416464"]);
+db.run('INSERT INTO planetes(name,size) values (?,?)', ["Jupiter", "46554614"]);
+db.run('INSERT INTO planetes(name,size) values (?,?)', ["Mercure","4654664"]);
+db.run('DELETE FROM planetes WHERE rowid = 3');
+
+
+db.all('SELECT * FROM planetes', (err, data) =>{
+ if (err) throw err; console.log(data);
+});

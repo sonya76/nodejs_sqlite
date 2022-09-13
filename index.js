4721794d6c692e34ff6b174c6bff0dbe51156096
Sonia Zaïna CHALAL <soniachalalceppic@gmail.com>
2022-09-13 16:57:17 +0200

    requete all + get + each

--- index.js
+++ index.js
@@ -14,9 +14,19 @@ db.run('INSERT INTO planetes(name,size) values (?,?)', ["Terre", "8516975125"]);
 db.run('INSERT INTO planetes(name,size) values (?,?)', ["Pluton","65416464"]);
 db.run('INSERT INTO planetes(name,size) values (?,?)', ["Jupiter", "46554614"]);
 db.run('INSERT INTO planetes(name,size) values (?,?)', ["Mercure","4654664"]);
-db.run('DELETE FROM planetes WHERE rowid = 3');
+//db.run('DELETE FROM planetes WHERE rowid = 3');
 
+// db.All : Affiche tout les enregistrements on ne peut afficher une ligne ou un champs demandé
+// db.all('SELECT * FROM planetes', (err, data) =>{
+//  if (err) throw err; console.log(data);
+// });
 
-db.all('SELECT * FROM planetes', (err, data) =>{
- if (err) throw err; console.log(data);
-});
+// db.each : Affiche les champs demandés
+// db.each('SELECT * FROM planetes', (err, data) =>{
+//  if (err) throw err; console.log(data.name, data.created_at);
+// });
+
+// db.get : Affiche la 1ere ligne
+db.get('SELECT * FROM planetes', (err, data) =>{
+    if (err) throw err; console.log(data.name, data.created_at);
+   });
